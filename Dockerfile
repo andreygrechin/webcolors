95c89c2ab9d82d5829b324cb314e878e181b166e
Andrey Grechin <36399892+andreygrechin@users.noreply.github.com>
2025-08-17 04:10:56 +0400

    refactor: 0.2.0

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,39 @@
-FROM python:3.11.3-alpine3.17
+ARG UV_IMAGE=ghcr.io/astral-sh/uv:0.8.11
+ARG BASE_IMAGE=python:3.13.7-alpine3.22
+
+# hadolint ignore=DL3006
+FROM $UV_IMAGE AS uv
+
+# hadolint ignore=DL3006
+FROM $BASE_IMAGE AS builder
+
+COPY --from=uv /uv /uvx /bin/
+WORKDIR /app
+RUN --mount=type=cache,target=/root/.cache/uv \
+    --mount=type=bind,source=uv.lock,target=uv.lock \
+    --mount=type=bind,source=pyproject.toml,target=pyproject.toml \
+    uv sync --frozen --no-install-project --no-dev
+COPY ./src/ src/
+COPY ./LICENSE .
+COPY ./pyproject.toml .
+COPY ./uv.lock .
+
+RUN --mount=type=cache,target=/root/.cache/uv \
+    uv sync --frozen --no-dev
+
+# ARG BASE_IMAGE=python:3.13.7-alpine3.22
+# hadolint ignore=DL3006
+FROM $BASE_IMAGE
+
+ARG APP_NAME=jobot
 ARG VERSION_TAG
 ARG GITHUB_ACTIONS
 ARG GITHUB_SHA
 ARG GITHUB_REF
 ARG RUNNER_ARCH
 ARG RUNNER_OS
+ARG WEBCOLORS_HOST
+ARG WEBCOLORS_PORT
 
 ENV VERSION_TAG=${VERSION_TAG:-N/A}
 ENV GITHUB_ACTIONS=${GITHUB_ACTIONS:-N/A}
@@ -12,22 +41,16 @@ ENV GITHUB_SHA=${GITHUB_SHA:-N/A}
 ENV GITHUB_REF=${GITHUB_REF:-N/A}
 ENV RUNNER_ARCH=${RUNNER_ARCH:-N/A}
 ENV RUNNER_OS=${RUNNER_OS:-N/A}
+ENV APP_NAME=webcolors
+ENV WEBCOLORS_HOST=${WEBCOLORS_HOST:-0.0.0.0}
+ENV WEBCOLORS_PORT=${WEBCOLORS_PORT:-8080}
+ENV PYTHONPATH=/app/src
 
-LABEL env=test \
-    os=alpine \
-    app=webcolors
 EXPOSE 8080
 WORKDIR /app
-COPY ./requirements.txt ./
-RUN /usr/local/bin/python -m pip install --upgrade pip --no-cache-dir \
-    && pip install --no-cache-dir -r requirements.txt \
-    && mkdir /app/logs \
-    && chown 1000:2000 /app/logs
-COPY ./src/ ./src/
-USER 1000:2000
-ENTRYPOINT ["python", "/app/src/webcolors.py"]
-# CMD ["--color", "gray"]
 
-# different ways to provide environment variables and arguments
-# ENTRYPOINT ["python", "/app/src/webcolors.py", "--color", "green"]
-# ENV COLOR=red
+COPY --from=builder /app /app
+
+RUN adduser -D -u 1000 -s /sbin/nologin "$APP_NAME"
+USER "$APP_NAME"
+CMD ["/app/.venv/bin/python3", "/app/src/webcolors/app.py"]
--- test.sh
+++ test.sh
@@ -4,19 +4,37 @@ set -e # exit if a command fails
 set -o pipefail # fail a pipeline if any command of pipeline failed
 
 image_name=$APP_NAME
-version="$(cat VERSION)"
-image_tag=${version//+/-}
-echo Image name: "${image_name}:${image_tag}"
+version="$(cat src/"${APP_NAME}"/VERSION)"
+image_tag="${version//+/-}"
+BASE_PYTHON_IMAGE_TAG="3.13.7-alpine3.22"
+timestamped_image="${image_name}:${image_tag}-$(date -u '+%Y%m%dT%H%M%SZ')"
+echo "Image name: ${timestamped_image}"
 
-python3 scripts/update_requirements.py
 
 docker run --rm --interactive --pull always hadolint/hadolint < Dockerfile
 
-docker build --build-arg VERSION_TAG="${image_tag}" --tag "${image_name}:${image_tag}" .
-echo docker build --build-arg VERSION_TAG="${image_tag}" --tag "${image_name}:${image_tag}" .
-docker run --detach --publish 8080:8080 --name "${image_name}" "${image_name}:${image_tag}"
-echo docker run --detach --publish 8080:8080 --name "${image_name}" "${image_name}:${image_tag}"
+docker build \
+    --build-arg VERSION_TAG="${image_tag}" \
+    --build-arg BASE_PYTHON_IMAGE_TAG="${BASE_PYTHON_IMAGE_TAG}" \
+    --tag "${timestamped_image}" \
+    --platform linux/amd64,linux/arm64 \
+    --annotation "org.opencontainers.image.created=$(date -u '+%Y-%m-%dT%H:%M:%SZ')" \
+    --annotation "org.opencontainers.image.url=https://github.com/andreygrechin/webcolors" \
+    --annotation "org.opencontainers.image.documentation=https://github.com/andreygrechin/webcolors/blob/main/README.md" \
+    --annotation "org.opencontainers.image.version=$(git describe --tags --always --dirty)" \
+    --annotation "org.opencontainers.image.revision=$(git rev-parse --short HEAD)" \
+    --annotation "org.opencontainers.image.source=$(git config --get remote.origin.url)" \
+    --annotation "org.opencontainers.image.title=${APP_NAME}" \
+    --annotation "org.opencontainers.image.description='An example of a containerized Flask app to play with K8s and Docker'" \
+    --annotation "org.opencontainers.image.authors=$(git config --get user.email)" \
+    --annotation "org.opencontainers.image.licenses=MIT" \
+    --annotation "org.opencontainers.image.base.name=docker.io/python:${BASE_PYTHON_IMAGE_TAG}" \
+    .
+container_id=$(docker run --detach --publish 8080:8080 "${timestamped_image}")
+echo "Container ID: ${container_id}"
 open http://localhost:8080/
-docker exec -it "${image_name}" sh
-docker rm --force "${image_name}"
-docker rmi "${image_name}:${image_tag}"
+docker exec -it "${container_id}" sh
+docker rm --force "${container_id}"
+echo "Deleted container ${container_id}"
+docker rmi "${timestamped_image}"
+echo "Deleted image ${timestamped_image}"
